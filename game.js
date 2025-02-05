614b3d46d45efbb3d70167d2361b3763dc0bc9bc
muzzyratti <186625828+muzzyratti@users.noreply.github.com>
2025-02-05 07:38:43 +0300

    feat: add js function for playing one round

--- game.js
+++ game.js
@@ -31,7 +31,7 @@ let humanChoice = "";
 function getHumanChoice () {
     humanChoice = prompt("What is your choice? (choose only from: rock, paper or scissors)").toLowerCase();
 
-    if (humanChoice === 'rock'|| humanChoice === 'paper'|| humanChoice === 'rock') {
+    if (humanChoice === 'rock'|| humanChoice === 'paper'|| humanChoice === 'scissors') {
         return humanChoice = String(humanChoice).charAt(0).toUpperCase() + String(humanChoice).slice(1);
     } else {
         alert ("Please choose only from: rock, paper or scissors.");
@@ -42,12 +42,30 @@ function getHumanChoice () {
 getHumanChoice();
 console.log(`Human choice: ${humanChoice}`);
 
-// Calculate result
+// Initiate players scores
+let humanScore = 0;
+let computerScore = 0;
 
-function playRound () {
-    //code
+// Playing 1 round
+function playRound (humanSelection, computerSelection) {
+    if (humanSelection == computerSelection) {
+        return alert('It is a draw!')
+    } else if (humanSelection == "Rock" && computerSelection == "Paper") {
+        return alert('You lose! Paper beats Rock.');
+    } else if (humanSelection == "Rock" && computerSelection == "Scissors") {
+        return alert('You win! Rock beats Scissors.');
+    } else if (humanSelection == "Paper" && computerSelection == "Rock") {
+        return alert('You win! Paper beats Rock.');
+    } else if (humanSelection == "Paper" && computerSelection == "Scissors") {
+        return alert('You lose! Scissors beats Paper.');
+    } else if (humanSelection == "Scissors" && computerSelection == "Rock") {
+        return alert('You lose! Rock beats Scissors.');
+    } else if (humanSelection == "Scissors" && computerSelection == "Paper") {
+        return alert('You win! Scissors beats Paper.');
+    }
 }
 
+console.log(playRound(humanChoice, computerChoice));
 
 // Show result of round and game
 
